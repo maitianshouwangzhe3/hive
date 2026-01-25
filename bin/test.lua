288c4e951278cc6e30c5b67fc9f2d58bd5731137
maitianshouwangzhe3 <2681457694@qq.com>
2026-01-25 18:46:31 +0800

    bugfix

--- bin/test.lua
+++ bin/test.lua
@@ -2,16 +2,16 @@
 
 ct = ct or 0;
 require "base/descriptor_pb"
-socket_mgr = hive.create_socket_mgr(100, 1024 * 1024, 1024 * 8);
+-- socket_mgr = hive.create_socket_mgr(100, 1024 * 1024, 1024 * 8);
 
-stream = socket_mgr.connect("127.0.0.1", 7571);
+-- stream = socket_mgr.connect("127.0.0.1", 7571);
 pb = require "pb"
 pb.load(descriptor_pb)
 print(pb)
 function hive.run()
     hive.sleep_ms(1000);
-	socket_mgr.wait(1000);
-	stream.call("login_req")
+	-- socket_mgr.wait(1000);
+	-- stream.call("login_req")
     print("ct="..ct);
     ct = ct + 1;
 	if ct > 10 then
